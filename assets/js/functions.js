b0e0503d484b4d6b394140c13ce94b517a9eaf43
James Hall-Treworgy <James Hall-Treworgy>
2016-04-01 00:09:50 -0400

    Added styling and a workaround using jquery for a strange fixed mobile navbar menu

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,27 @@
 $(document).ready(function(){
 
-  $('#nav-icon').on('click', function() {
+  $('#nav-icon').on('click', function( event ) {
+    //
+    // var s-ic  = '.services-intro-container';
+    // var s-mc = '.services-message-container';
+    // var iic = '.index-intro-container';
+    // var s-smic = '.services-middle-container';
+
 
     $('#nav-icon span').toggleClass('open');
+    $('.services-intro-container, .services-message-container, .index-intro-container, .services-middle-container').toggleClass('slide-open');
 
-    $('body').toggleClass('slide-open');
+    event.stopPropagation();
+    event.preventDefault();
+  });
+
+  $(window).on('scroll', function() {
+    var scrollCheck = $(this).scrollTop();
+
+
+    $('#mobile-nav-wrapper').css({
+      'top' : scrollCheck + 'px'
+    });
 
   });
 
